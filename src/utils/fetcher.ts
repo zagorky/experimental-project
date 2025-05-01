e88d52f22aa6b1cc91288cf157814fa420ece4dd
zagorky <zagorkiynahyi@gmail.com>
2025-05-01 23:29:13 +0300

    fix: fix posts page error

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,13 +1,13 @@
-// export const fetcher = async <T>(input: RequestInfo, init?: RequestInit): Promise<T> => {
-//   const res = await fetch(input, init);
-//   const data: unknown = await res.json();
-//   return data as T;
-// };
-
-import { ZodType } from 'zod';
-
-export async function fetcher<S>(schema: ZodType<S, any>, input: RequestInfo, init?: RequestInit): Promise<S> {
+export const fetcher = async <T>(input: RequestInfo, init?: RequestInit): Promise<T> => {
   const res = await fetch(input, init);
-  const json: unknown = await res.json();
-  return schema.parse(json);
-}
+  const data: unknown = await res.json();
+  return data as T;
+};
+
+// import { ZodType } from 'zod';
+//
+// export async function fetcher<S>(schema: ZodType<S, any>, input: RequestInfo, init?: RequestInit): Promise<S> {
+//   const res = await fetch(input, init);
+//   const json: unknown = await res.json();
+//   return schema.parse(json);
+// }
