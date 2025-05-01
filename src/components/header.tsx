574ce4fea3705ef9110656d747202da83378c1d0
zagorky <zagorkiynahyi@gmail.com>
2025-05-01 19:21:52 +0300

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '~components/ui/navigation-menu.tsx';
+import { Link } from 'react-router';
+
+export const Header = () => {
+  return (
+    <NavigationMenu className={'m-auto p-4'}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link to="/">Main</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link to="/state">UseState</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link to="/signals">Signals</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link to="/shadcn">Shadcn</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,24 +1,10 @@
-import { Link, Outlet } from 'react-router';
+import { Outlet } from 'react-router';
+import { Header } from '~components/header.tsx';
 
 export const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Main</Link>
-          </li>
-          <li>
-            <Link to="/state">UseState</Link>
-          </li>
-          <li>
-            <Link to="/signals">Signals</Link>
-          </li>
-          <li>
-            <Link to="/shadcn">Shadcn</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header />
       <Outlet />
     </>
   );
