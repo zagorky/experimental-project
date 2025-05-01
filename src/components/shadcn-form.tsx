6020d895ae46043db1fb38da24fa4b1c977f5771
zagorky <zagorkiynahyi@gmail.com>
2025-05-01 23:48:27 +0300

    fix: shadcn form error

--- src/components/shadcn-form.tsx
+++ src/components/shadcn-form.tsx
@@ -31,7 +31,7 @@ export const ShadcnForm = () => {
     <>
       <h2 className={'font-bold text-pink-700 text-2xl'}>Shadcn Form</h2>
       <Form {...form}>
-        <form className={'p-4'} onSubmit={(e) => void form.handleSubmit(onSubmit)(e)}>
+        <form className={'p-4'} onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
             name={'name'}
             control={form.control}
