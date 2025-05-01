e116f57572dca53486f817d787557441ca0680e7
zagorky <zagorkiynahyi@gmail.com>
2025-05-01 15:22:11 +0300

    feat: add shadcn form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,9 @@
 import './App.css';
-import { SignalForm } from '~components/signal-form.tsx';
-import { UseStateForm } from '~components/use-state-form.tsx';
 import { ShadcnForm } from '~components/shadcn-form.tsx';
 
 function App() {
   return (
     <section>
-      <SignalForm />
-      <UseStateForm />
       <ShadcnForm />
     </section>
   );
---
+++ src/components/shadcn-form.tsx
@@ -0,0 +1,61 @@
+import { useForm } from 'react-hook-form';
+import { UserFormDataType } from '~types/types.ts';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { UserFormData } from '~types/schemas.ts';
+import { useUsersStore } from '~stores/users-store.ts';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '~components/ui/form.tsx';
+import { Input } from '~components/ui/input.tsx';
+import { Button } from '~components/ui/button.tsx';
+
+export const ShadcnForm = () => {
+  const addUser = useUsersStore((state) => state.addUser);
+
+  const form = useForm<UserFormDataType>({
+    resolver: zodResolver(UserFormData),
+    defaultValues: {
+      name: '',
+      email: '',
+    },
+  });
+
+  const onSubmit = (data: UserFormDataType) => {
+    console.log(data);
+    addUser(data);
+  };
+
+  console.log('Shadcn form render');
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          name={'name'}
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        ></FormField>
+        <FormField
+          name={'email'}
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        ></FormField>
+        <Button type={'submit'}>Submit</Button>
+      </form>
+    </Form>
+  );
+};
