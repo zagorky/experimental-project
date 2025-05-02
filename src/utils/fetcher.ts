c412874aff0c07e5b69e813dfc8bb8949dea0e3e
zagorky <zagorkiynahyi@gmail.com>
2025-05-02 20:43:12 +0300

    refactor: delete clog

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -4,6 +4,5 @@ export const fetcher = async <T>(input: RequestInfo, init?: RequestInit): Promis
     throw new Error(`Request failed with status ${res.status.toString()}`);
   }
   const data: unknown = await res.json();
-  console.log('Fetcher response:', data);
   return data as T;
 };
