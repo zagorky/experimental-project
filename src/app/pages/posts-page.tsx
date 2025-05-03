7461f8265dd575b4f0fb704d21e737954605be80
zagorky <zagorkiynahyi@gmail.com>
2025-05-03 16:43:46 +0300

    feat: try to use useDeferredValue and useTransition hooks but not successful

--- src/app/pages/posts-page.tsx
+++ src/app/pages/posts-page.tsx
@@ -4,9 +4,29 @@ import { fetcher } from '~utils/fetcher.ts';
 import { PostType } from '~types/types.ts';
 import { Loader } from '~components/ui/loader.tsx';
 import { ZodError } from 'zod';
+import { useDeferredValue, useState, useTransition } from 'react';
+import { Input } from '~components/ui/input.tsx';
+import { Label } from '~components/ui/label.tsx';
 
 export const PostsPage = () => {
   const { data, error, isLoading } = useSWR<PostType[], ZodError>(POSTS_ENDPOINT, fetcher);
+  const [query, setQuery] = useState('');
+  const [isPending, startTransition] = useTransition();
+  const deferredQuery = useDeferredValue(query);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    startTransition(() => {
+      setQuery(event.target.value);
+    });
+  };
+
+  const filteredPosts =
+    data?.filter((post) => {
+      if (!deferredQuery) {
+        return true;
+      }
+      return post.title.toLowerCase().includes(deferredQuery.toLowerCase());
+    }) ?? [];
 
   return (
     <>
@@ -16,16 +36,27 @@ export const PostsPage = () => {
       {!data ? (
         <div>Nothing to render</div>
       ) : (
-        <ul>
-          {data.map((post) => {
-            return (
-              <li key={post.id}>
-                <h3 className={'font-bold'}>{post.title}</h3>
-                <p>{post.body}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          <Label>
+            Search
+            <Input value={deferredQuery} onChange={handleChange} />
+          </Label>
+          {isPending ? (
+            <p>Loading...</p>
+          ) : (
+            <ul className={'max-w-150'}>
+              {filteredPosts.map((post) => {
+                return (
+                  <li key={post.id}>
+                    <h3 className={'font-bold'}>{post.userId}</h3>
+                    <h4 className={'font-bold'}>{post.title}</h4>
+                    <p>{post.body}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </>
       )}
     </>
   );
