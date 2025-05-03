5677a4c76569a051783d8d5fb4a773a7d3a3f08b
zagorky <zagorkiynahyi@gmail.com>
2025-05-03 16:43:05 +0300

    feat: add suspense

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 import { Layout } from '~components/layouts/layout.tsx';
 import { pathes } from '~config/routes-config.ts';
-import { Loader } from '~components/ui/loader.tsx';
 
 export const AppRouter = createBrowserRouter([
   {
@@ -9,7 +8,6 @@ export const AppRouter = createBrowserRouter([
     children: [
       {
         path: pathes.main,
-        HydrateFallback: Loader,
         async lazy() {
           const { MainPage } = await import('./pages/main-page.tsx');
 
@@ -20,7 +18,6 @@ export const AppRouter = createBrowserRouter([
       },
       {
         path: pathes.state,
-        HydrateFallback: Loader,
         async lazy() {
           const { UseStatePage } = await import('./pages/use-state.page.tsx');
 
@@ -31,7 +28,6 @@ export const AppRouter = createBrowserRouter([
       },
       {
         path: pathes.signals,
-        HydrateFallback: Loader,
         async lazy() {
           const { SignalsPage } = await import('./pages/signals-page.tsx');
 
@@ -42,7 +38,6 @@ export const AppRouter = createBrowserRouter([
       },
       {
         path: pathes.shadcn,
-        HydrateFallback: Loader,
         async lazy() {
           const { ShadcnPage } = await import('./pages/shadcn-page.tsx');
 
@@ -53,7 +48,6 @@ export const AppRouter = createBrowserRouter([
       },
       {
         path: pathes.posts,
-        HydrateFallback: Loader,
         async lazy() {
           const { PostsPage } = await import('./pages/posts-page.tsx');
 
@@ -64,7 +58,6 @@ export const AppRouter = createBrowserRouter([
       },
       {
         path: pathes['async-race'],
-        HydrateFallback: Loader,
         async lazy() {
           const { AsyncRacePage } = await import('./pages/async-race-page.tsx');
 
--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router';
 import { Header } from '~components/header.tsx';
+import { Suspense } from 'react';
+import { Loader } from '~components/ui/loader.tsx';
 
 export const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </>
   );
 };
