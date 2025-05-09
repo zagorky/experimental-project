4c81205c1ca12d9c48e3004f4bb8b136e9443b7b
zagorky <zagorkiynahyi@gmail.com>
2025-05-09 21:39:40 +0300

    feat: add tests

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vitest/config';
+import config from './vite.config';
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
+
+export default mergeConfig(
+  config,
+  defineConfig({
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './vitest.setupTests.ts',
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json-summary'],
+        reportsDirectory: './coverage',
+        include: ['src/**/*.{ts,tsx}'],
+        exclude: ['node_modules/**'],
+        all: true,
+      },
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
+      },
+    },
+  }),
+);
